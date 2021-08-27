17c8b9f6f78e1f24bc5db3247b55f1224becf4cc
hrincredible007 <59758425+hrincredible007@users.noreply.github.com>
2021-08-27 13:27:54 +0530

    Payement Without Changing

---
+++ 3]. Competitive Programming/CodeForces/1]. Problem Set/Levels/A/PaymentWithoutChange.java
@@ -0,0 +1,10 @@
+package Divivsion_A;
+
+import java.util.Scanner;
+
+public class PaymentWithoutChange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
